fd114557f5b237ec6588255038da8fabfd2f3180
farahbousselmi <53794189+farahbousselmi@users.noreply.github.com>
2019-09-03 12:11:40 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export let moviesToDisplay = [
+  {
+    id: "movie1",
+    title: "Berofe sunrise",
+    rating: 5,
+    image: "http://www.gstatic.com/tv/thumb/v22vodart/16390/p16390_v_v8_aa.jpg",
+    year: 1995,
+    trailer: "AtwRBjmUHX8"
+  },
+  {
+    id: "movie2",
+    title: "Before sunset",
+    rating: 3,
+    image:
+      "http://fr.web.img4.acsta.net/medias/nmedia/18/35/22/35/18409539.jpg",
+    year: 2004,
+    trailer: "hTtJX-4Kh_4"
+  },
+  {
+    id: "movie3",
+    title: "Before midnight",
+    rating: 4,
+    image:
+      "http://fr.web.img5.acsta.net/pictures/210/123/21012323_20130613143708915.jpg",
+    year: 2013,
+    trailer: "N3ijJH6PllQ"
+  }
+];
